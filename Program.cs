c850b03f3b22fded2260e9f17c88d2059e1b3b8b
Anastasiia Kuzmina <a.p.kuzmina@gmail.com>
2021-02-21 18:47:49 +0200

    Init commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Min_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[10]; //объявление массива 
+            Console.WriteLine("Enter 10 integers: ");
+
+            for (int i = 0; i < 10; i++)
+            {
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                System.Console.WriteLine("The " + i + " integer is " + a[i]);
+            }
+            int res = search_min (a);
+            System.Console.WriteLine($"Result is: {res}");
+        }
+
+        static int search_min(int[] b)
+        {
+            int min = b[0];
+            for (int i = 1; i < 10; i++)
+            {
+                if (b[i] < min)
+                {
+                 min = b[i];
+                }
+             }
+            return min;
+        }
+    }
+}
